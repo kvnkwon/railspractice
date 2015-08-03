59219a47e2eb61c6681c176f40f894be2cf7c81f
Kevin <kvnkwon@gmail.com>
2015-08-03 17:16:11 -0400

    Migrate items

--- foodify/db/schema.rb
+++ foodify/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803195856) do
+ActiveRecord::Schema.define(version: 20150803210328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name",        null: false
+    t.integer  "playlist_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "playlists", force: :cascade do |t|
     t.string   "name",       null: false
