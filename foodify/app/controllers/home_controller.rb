eb340ed5f47e0f18c0b07d16fbf42ecad889e16a
Kevin <kvnkwon@gmail.com>
2015-08-04 15:09:51 -0400

    Hide api keys

--- foodify/app/controllers/home_controller.rb
+++ foodify/app/controllers/home_controller.rb
@@ -3,4 +3,11 @@ class HomeController < ApplicationController
   def index
   end
 
+  def search
+    location = params[:location]
+    parameters = { term: params[:term], limit: 5 }
+    yelp_search = Yelp.client.search(location, parameters)
+    @results = yelp_search.businesses
+  end
+
 end
